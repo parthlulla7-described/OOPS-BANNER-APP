d925dbbe53003af61036fa8c252c2deae1027f09
parthlulla7-described <parthlulla7@gmail.com>
2026-03-11 08:38:00 +0530

    UC3 Banner Assignment

---
+++ OOPSBannerJoin.java
@@ -0,0 +1,55 @@
+public class OOPSBannerJoin {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                " *****   ",
+                " *****   ",
+                " ******  ",
+                "  ***** "
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *  ",
+                "*     *  ",
+                "*     *  ",
+                " *      "
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *  ",
+                "*     *  ",
+                "*     *  ",
+                " *      "
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *  ",
+                "*     *  ",
+                " ******  ",
+                "  ***** "
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *  ",
+                "*     *  ",
+                "*        ",
+                "       *"
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *  ",
+                "*     *  ",
+                "*        ",
+                "       *"
+        ));
+
+        System.out.println(String.join(" ",
+                " *****   ",
+                " *****   ",
+                "*        ",
+                "  ***** "
+        ));
+
+    }
+}
